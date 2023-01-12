54188b39e82f385b804048d041f277afa9e36aa9
Krzysztof Sidor <ksidor93@gmail.com>
2023-01-12 18:59:17 +0100

    initial commit

---
+++ src/main/java/pl/emotnazysta/warsztatydocker/WarsztatyDockerApplication.java
@@ -0,0 +1,13 @@
+package pl.emotnazysta.warsztatydocker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WarsztatyDockerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WarsztatyDockerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/pl/emotnazysta/warsztatydocker/WarsztatyDockerApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.emotnazysta.warsztatydocker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WarsztatyDockerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
